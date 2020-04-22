60095c7994224ace0e8717020758cb56112196f9
j770044 <j770044@gmail.com>
2020-04-22 19:47:07 +0530

    Initial commit

---
+++ calc simple.py
@@ -0,0 +1,14 @@
+# trying to learn python
+
+print("Hello, world")
+
+# Trying few examples in python
+
+if 5>2:
+	print("five is grater than two")
+
+if 5>2:
+ print("two is less than five")
+
+ if 5>2:
+ 	print("five is greater than two")
